690e7545c6608265d61af079718adada04977d54
vignesh <palanivig12@gmail.com>
2023-09-20 20:23:24 +0530

    commit

---
+++ sumofn.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class sumofn {
+    public void sum(int n)
+    {
+        System.out.println((n*(n+101))/2);
+    }
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sumofn obj = new sumofn();
+        obj.sum(n);
+
+    }
+    
+    
+}

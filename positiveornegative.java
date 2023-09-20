1b7612938f72859f73fae094b43a912c825d3a9f
vignesh <palanivig12@gmail.com>
2023-09-20 19:06:32 +0530

    commit

--- positiveornegative.java
+++ positiveornegative.java
@@ -2,15 +2,16 @@ import java.util.*;
 class positiveornegative{
     public static void main(String args[])
     {
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        if(num==0)
-        {
-            System.out.println("Zero");
-        }
-        else{
-            String s = num>0 ? "Positive" : "Negative";
-            System.out.println(s);
+        try (Scanner sc = new Scanner(System.in)) {
+            int num = sc.nextInt();
+            if(num==0)
+            {
+                System.out.println("Zero");
+            }
+            else{
+                String s = num>0 ? "Positive" : "Negative";
+                System.out.println(s);
+            }
         }
     }
 }

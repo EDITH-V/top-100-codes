7eeaf8f7aca3f3a7e9641de681aae9e03b408258
vignesh <palanivig12@gmail.com>
2023-09-22 15:43:47 +0530

    commit

---
+++ prime.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class prime{
+    static int flag=1;
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            int num = sc.nextInt();
+            
+            // if(num==2)
+            //     System.out.println("prime");
+            for(int i=2;i<Math.sqrt(num);i++)
+            {
+                if(num%i==0){
+                    flag++;
+                    break;
+                }
+                else
+                    continue;
+            }
+            if(flag==1)
+            System.out.println("prime");
+            else
+            System.out.println("Not prime");
+        }
+
+    }
+}
